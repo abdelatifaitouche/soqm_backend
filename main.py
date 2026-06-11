dd29fcfc3c6778e1418c622071f4cb684c9c931b
Abdelatif Aitouche <abdelatif.aitouche3@gmail.com>
2026-06-11 02:05:17 +0200

    feat(cors): add cors middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.router import router as api
 from src.infra.db import models
 from src.api.exceptions_handlers import register_exception_handlers
@@ -6,7 +7,20 @@ from src.api.exceptions_handlers import register_exception_handlers
 
 def create_app() -> FastAPI:
 
+    origins = [
+        "http://localhost:5173",
+        "http://localhost",
+    ]
+
     app = FastAPI(title="SOQM", version="v1")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     register_exception_handlers(app)
 
